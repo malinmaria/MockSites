c9c81473cc6bffc2365b6df74bbd23e9061a7d6b
Malin Nemergut <mmheineking@gmail.com>
2016-11-03 17:50:16 -0400

    using jquery to move cookie imgs

--- DessertCity/js/app.js
+++ DessertCity/js/app.js
@@ -1,1 +1,18 @@
 $(".warning").hide().show("slow");
+
+    $(".cookies").click(function(){
+      $("#box").toggle(2000);
+    });
+
+    $(".c-cookie").click(function(){
+      $(".c-cookie").animate({
+        right: '250px'
+      });
+    });
+
+    $(".m-cookie").click(function(){
+      $(".m-cookie").animate({
+        right: '250px'
+      });
+    });
+    
